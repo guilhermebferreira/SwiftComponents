3573d27adfb942c9c7c17caddb2f8edb625888c1
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-24 14:58:43 -0300

    habilitando e desabilitando componentes com o switch, trocando a cor da tela com o segmente e escondendo os items da tela a partir de um toque

--- Components/ViewController.swift
+++ Components/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var vrSegmented: UISegmentedControl!
+    @IBOutlet weak var vrSlider: UISlider!
+    @IBOutlet weak var vrImage: UIImageView!
+    @IBOutlet weak var vrSwitch: UISwitch!
+    @IBOutlet weak var vrLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,11 +25,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //trata um toque na tela
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        vrLabel.isHidden = !vrLabel.isHidden
+        vrSwitch.isHidden = !vrSwitch.isHidden
+        vrImage.isHidden = !vrImage.isHidden
+        vrSegmented.isHidden = !vrSegmented.isHidden
+        vrSlider.isHidden = !vrSlider.isHidden
+    }
+    
     //trata o evento de alteração de valor do switch
     @IBAction func handleSwitch(_ sender: UISwitch) {
+        vrSegmented.isEnabled = vrSwitch.isOn
+        vrSlider.isEnabled = vrSwitch.isOn
     }
     //trata o evento de alteração do valor do segmented
     @IBAction func handleSegmented(_ sender: UISegmentedControl) {
+        switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.view.backgroundColor = UIColor.red
+        case 1:
+            self.view.backgroundColor = UIColor.green
+        case 2:
+            self.view.backgroundColor = UIColor.purple
+        case 3:
+            self.view.backgroundColor = UIColor.yellow
+        default:
+            self.view.backgroundColor = UIColor.white
+
+        }
     }
     //trata o evento de alteração do slider
     @IBAction func handleSlider(_ sender: UISlider) {
